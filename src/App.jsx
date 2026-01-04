018b9cc6766d7b4473b41ddfb6372cd2c4920df7
Alex Brilevski <brilevski.alex@gmail.com>
2026-01-04 12:27:20 +0300

    Redirect and data refresh after creating a new event were implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import {
   Navigate,
   RouterProvider,
   createBrowserRouter,
 } from 'react-router-dom';
+import { queryClient } from './util/http.js';
 
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
@@ -37,8 +38,6 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
