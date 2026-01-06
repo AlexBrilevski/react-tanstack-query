b2588bb41377ce98830259bb56c3665cecc0e4a8
Alex Brilevski <brilevski.alex@gmail.com>
2026-01-06 12:10:49 +0300

    Optimistic UI update after updating an event was implemented

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { fetchEvent, updateEvent } from '../../util/http.js';
+import { fetchEvent, queryClient, updateEvent } from '../../util/http.js';
 
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import Modal from '../UI/Modal.jsx';
@@ -18,6 +18,21 @@ export default function EditEvent() {
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ['events', params.id] });
+      const previousEvent = queryClient.getQueryData(['events', params.id]);
+      queryClient.setQueryData(['events', params.id], newEvent);
+
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', params.id], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['events', params.id] });
+    },
   });
 
   function handleSubmit(formData) {
